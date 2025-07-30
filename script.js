3bb5da23c02ab73c09d54365b7dcfddd89a973a0
monirulislamkhan <mr.monirulkhan@gmail.com>
2025-07-30 07:39:32 +0530

    Shopping item add work done

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ const itemForm = document.getElementById('item-form');
 const addInput = document.getElementById('item-input');
 const ulList = document.getElementById('item-list');
 
-// Create function
-
-/* addInput.addEventListener('keyup', function (e) {
-  console.log(e.target);
-  document.querySelector('h1').textContent = e.target.value;
-}); */
 itemForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  // console.log(e.target);
-  console.log(e.target);
+  const value = addInput.value.trim();
+
+  if (!value) {
+    alert('Please enter value');
+    addInput.focus();
+    return;
+  }
 
   // Create li element
   const li = document.createElement('li');
-  // li.appendChild(document.createTextNode());
+  // li.append(value);
+  li.appendChild(document.createTextNode(value));
+
+  // Create button element
+  const btn = document.createElement('button');
+  btn.className = 'remove-item btn-link text-red';
+  btn.setAttribute('type', 'button');
+  btn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+  // li.append(btn);
+  li.appendChild(btn);
+
+  ulList.appendChild(li);
+  addInput.value = '';
+  addInput.focus();
 });
